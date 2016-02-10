f5af20dbbfbeda09e933b3e5bf049897285d2ed8
Jackson Hamner <jacksonhamner@gmail.com>
2016-02-10 13:36:33 -0800

    Initial commit

---
+++ ColorPicker/ColorPicker.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ColorPicker
+{
+    partial class ColorPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColorPicker));
+            this.lblHEX = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRGB = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHEX
+            // 
+            this.lblHEX.AutoSize = true;
+            this.lblHEX.Location = new System.Drawing.Point(47, 79);
+            this.lblHEX.Name = "lblHEX";
+            this.lblHEX.Size = new System.Drawing.Size(0, 13);
+            this.lblHEX.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HEX:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "RGB:";
+            // 
+            // lblRGB
+            // 
+            this.lblRGB.AutoSize = true;
+            this.lblRGB.Location = new System.Drawing.Point(47, 99);
+            this.lblRGB.Name = "lblRGB";
+            this.lblRGB.Size = new System.Drawing.Size(0, 13);
+            this.lblRGB.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(285, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Click this window and drag your mouse around the screen. ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(262, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "To set the color you want to pick release your mouse. ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 49);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(295, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "The HEX value will automatically be saved to your clipboard. ";
+            // 
+            // ColorPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 127);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblRGB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHEX);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ColorPicker";
+            this.Text = "Color Picker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHEX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRGB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ColorPicker/ColorPicker.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+The purpose of this project is to create a color picker that will grab the color values from wherever
+the user's mouse is hovering over. 
+*/
+
+namespace ColorPicker
+{
+
+    public partial class ColorPicker : Form
+    {
+        //Initialize
+        public ColorPicker()
+        {
+            InitializeComponent();
+            this.label5.AutoSize = true;        
+        }       
+        
+        //Run code when mouse moves.
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+
+            //Initialize list to hold RGB values
+            List<int> ARGBValues = new List<int>();
+
+            //Get positon of mouse
+            int XPos = Cursor.Position.X;
+            int YPos = Cursor.Position.Y;
+
+            //Calls the picker class which will return the ARGB value of the area under wherever the mouse is sitting
+            string cValue = picker.GetPixelColor(XPos, YPos).ToString();
+
+            //Split into list and remove all empty values
+            IList<string> values = cValue.Split(new String[] { ",", " ", "[", "]","=" }, StringSplitOptions.RemoveEmptyEntries);
+            
+            //Int used for loop
+            int intVal = 0;
+
+            //filter out all strings and pull just the four ARGB value codes
+            foreach (string word in values)
+            {
+                if (int.TryParse(word, out intVal))
+                {
+                    ARGBValues.Add(intVal);
+                }
+            }
+            
+            //change background color to current RGB value
+            this.BackColor = Color.FromArgb(ARGBValues[1], ARGBValues[2], ARGBValues[3]);
+
+            //If the brightness of the background color is above 130, set text color to black
+            if(Brightness(BackColor) > 130)
+            {
+                lblHEX.ForeColor = Color.Black;
+                label2.ForeColor = Color.Black;
+                label3.ForeColor = Color.Black;
+                lblRGB.ForeColor = Color.Black;
+                label5.ForeColor = Color.Black;
+                label6.ForeColor = Color.Black;
+                label7.ForeColor = Color.Black;
+            }
+            //If brightness is below 130 set text color to white
+            else
+            {
+                lblHEX.ForeColor = Color.White;
+                label2.ForeColor = Color.White;
+                label3.ForeColor = Color.White;
+                lblRGB.ForeColor = Color.White;
+                label5.ForeColor = Color.White;
+                label6.ForeColor = Color.White;
+                label7.ForeColor = Color.White;
+            }
+
+            //Update label with Hex Value
+            lblHEX.Text = RGB2Hex(ARGBValues[1], ARGBValues[2], ARGBValues[3]);
+            //Update label with RGB values
+            lblRGB.Text = ARGBValues[1].ToString() + ", " + ARGBValues[2].ToString() + ", " + ARGBValues[3].ToString();
+
+            //Save Hex value to clipboard 
+            Clipboard.SetText(RGB2Hex(ARGBValues[1], ARGBValues[2], ARGBValues[3]));
+
+            base.OnMouseMove(e);
+            
+        }
+
+        //Method to convert the RGB values into a HEX value
+        public string RGB2Hex(int r, int g, int b) {
+
+            string hex = string.Format("#{0}{1}{2}",
+                ((int)(r)).ToString("X2"),
+                ((int)(g)).ToString("X2"),
+                ((int)(b)).ToString("X2")
+                );
+            return hex;
+        }
+
+
+        //Check the brightness of the color. 
+        public int Brightness(Color c)
+        {
+            return (int)Math.Sqrt(
+                c.R * c.R * .299 +
+                c.G * c.G * .587 + 
+                c.B * c.B * .114);
+        }
+    }
+}
+
+
+
+
+
